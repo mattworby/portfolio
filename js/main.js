e049994f2986b01222ce54e230daca6a5ca90c9a
Matt W <mattworby@gmail.com>
2021-06-03 15:36:43 -0500

    First major commit
    Adding all folders, images, and code to projects

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function changePage(id){
+	switch (id){
+		case 'resumeNav':
+			window.open('file:///C:/Users/PC%20Master%20Race/Desktop/portfolio/Resume/resume.pdf');
+			break;
+		case 'linkedNav':
+			window.open('https://www.linkedin.com/in/mattworby13a87b123/')
+			break;
+		case 'github':
+			window.open('https://github.com/mattworby')
+			break;
+		default:
+			break;
+	}
+}
+
+function showText(item,show){
+	switch(item){
+		case 'twitch':
+			if(show == 'show'){
+				document.getElementById('workTwitchText').style.display = 'block';
+			} else {
+				document.getElementById('workTwitchText').style.display = 'none';
+			}
+			break;
+		case 'cat':
+			if(show == 'show'){
+				document.getElementById('workCatText').style.display = 'block';
+			} else {
+				document.getElementById('workCatText').style.display = 'none';
+			}
+			break;
+		case 'equation':
+			if(show == 'show'){
+				document.getElementById('workEquationText').style.display = 'block';
+			} else {
+				document.getElementById('workEquationText').style.display = 'none';
+			}
+			break;
+	}
+}
